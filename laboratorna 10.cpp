008baf6d12da507309560e8ef861e7ab2c6301b6
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-21 22:36:26 +0200

    Завдання 5

--- laboratorna 10.cpp
+++ laboratorna 10.cpp
@@ -1,12 +1,30 @@
 ﻿#include <iostream>
 #include <cmath>
-int main()
-{
-	setlocale(0, ".1251");
-	const double M = 3.8;
-	const double p = 2;
-	double t = exp(M + 1);
-	double x = pow(t, 2) + p;
-	double y = log(fabs(x + t));
-	std::cout << "Вiдповiдь y= " << y << std::endl;
+
+int main() {
+    setlocale(0, ".1251");
+
+
+    std::cout << "Введiть низку чисел для обчислення задачi" << std::endl;
+    double x;
+    std::cout << "Ведiть значення х: ";
+    std::cin >> x;
+    double n;
+    std::cout << "Ведiть значення n: ";
+    std::cin >> n;
+tryAgain:
+    double y;
+    std::cout << "Ведiть значення y: ";
+    std::cin >> y;
+
+    if (std::cos(y) == 0)
+    {
+        std::cout << "Косинус(y) не може дорiвнювати нулю. Змiнiть значення y";
+        goto tryAgain;
+    }
+    double result = x + (0.5 * n + 4.8) / std::cos(y);
+    std::cout << "Вiдповiддю на рiвняння є " << result;
+
+
+
 }
