271b2b7f301731e647d1352436945996be989d48
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-21 22:19:01 +0200

    Завдання 1.1

---
+++ laboratorna 10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+//Завдання 1.1
+    double p = 3.14;
+    std::cout << "Enter radiys: ";
+    int r;
+    std::cin >> r;
+    std::cout << "Enter action p/s: ";
+    char a;
+    std::cin >> a;
+    switch (a)
+    {
+    case('p'):
+        std::cout << "The radiys is: " << 2 * r * p;
+        break;
+    case('s'):
+        std::cout << "The square is: " << p * r * r;
+        break;
+    default:
+        std::cout << "Wrong answer";
+        break;
+    }
+}
