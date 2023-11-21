d7497f22a01444ef5876348a628d896b6910637f
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-21 22:26:26 +0200

    Завдання 2

--- laboratorna 10.cpp
+++ laboratorna 10.cpp
@@ -3,12 +3,12 @@
 int main()
 {
 	setlocale(0, ".1251");
-	const int t1 = 1;
-	const int t2 = 2;
-	int g;
-	std::cout << "Enter fastening: ";
-	std::cin >> g;
-	std::cout << "Пiсля 1 секунди тiло пройшло: " << (g * pow(t1, 2)) / 2 << std::endl;
-	std::cout << "Пiсля 2 секунд тiло пройшло: " << (g * pow(t2, 2)) / 2;
-
+	const int i = 16;
+	int xA = 0, yA = 0, xB = i, yB = i - 1, xC = -i, yC = i + 1;
+	double h = std::fabs(xA * (yB - yC) + xB * (yC - yA) + xC * (yA - yB)) / std::pow((xB - xC), 2) + std::pow((xB - xC), 2);
+	double AB_L = std::sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
+	double AC_L = std::sqrt((xC - xA) * (xC - xA) + (yC - yA) * (yC - yA));
+	double wA = (2 * AB_L * AC_L) / (AB_L + AC_L);
+	std::cout << "Висота: " << h << std::endl;
+	std::cout << "Бiсектриса: " << wA << std::endl;
 }
